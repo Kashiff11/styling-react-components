c25bd091f05fc05e8fd8ff4250df45e295bb84ce
Kashiff Khan <kashiff.k@yahoo.com>
2021-02-05 10:54:12 -0500

    Home page setup complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import {Link, Route, Switch } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <h2>
+      <h2 className="intro">
         Below are links to the completed examples from Jake Trent's
-        course on styling react components available on Pluralsight.com.
-        
+        course on styling react components available on Pluralsight.com. 
       </h2>
+      <div className="links">
+        <Link to='/inline' className="link">Inline Styling</Link>
+        <Link to='/css-in-js'className="link">CSS-in-JS</Link>
+        <Link to='css-stylesheets' className="link">CSS Stylesheets</Link>
+        <Link to='css-modules' className="link">CSS Modules</Link>
+      </div>
+
+      <Switch>
+        <Route exact path='/inline'><h1>inline</h1></Route>
+        <Route exact path='/css-in-js'><h1>css-in-js</h1></Route>
+        <Route exact path='/css-stylesheets'><h1>css-stylesheets</h1></Route>
+        <Route exact path='/css-modules'><h1>css-modules</h1></Route>
+      </Switch>
     </div>
   );
 }
